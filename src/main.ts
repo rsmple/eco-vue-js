b00a12db1a64d05186b0e5e66fdc9ba17f22d5b5
rsimple <texturered@gmail.com>
2023-02-10 20:47:08 +0100

    ui-kit: add utils imports

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,11 @@
 export * as iconsDefault from '@/imports/iconsDefault'
 export * as iconsSax from '@/imports/iconsSax'
 export * from '@/imports/componentsPlugin'
+
+export * from '@/utils/Notify'
+export * from '@/utils/Modal'
+export * from '@/utils/SemanticType'
+export * from '@/utils/HorizontalAlign'
+export * from '@/utils/dateTime'
+export * from '@/utils/mobile'
+export * from '@/utils/utils'
