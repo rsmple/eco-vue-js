86534d9ff2b085d124777e69728a56c04a4b8ed0
rsimple <texturered@gmail.com>
2023-02-12 16:43:09 +0100

    FRONT: add icon

--- src/imports/iconsSax.ts
+++ src/imports/iconsSax.ts
@@ -17,6 +17,7 @@ export {default as IconExport} from '@/assets/icons/sax/IconExport.svg?component
 export {default as IconEye} from '@/assets/icons/sax/IconEye.svg?component'
 export {default as IconFilter} from '@/assets/icons/sax/IconFilter.svg?component'
 export {default as IconFinding} from '@/assets/icons/sax/IconFinding.svg?component'
+export {default as IconFolder} from '@/assets/icons/sax/IconFolder.svg?component'
 export {default as IconJira} from '@/assets/icons/sax/IconJira.svg?component'
 export {default as IconLink} from '@/assets/icons/sax/IconLink.svg?component'
 export {default as IconLinkBold} from '@/assets/icons/sax/IconLinkBold.svg?component'
