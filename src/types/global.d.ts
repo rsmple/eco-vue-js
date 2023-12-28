0b7947e1995e519ec22711b599f16dc93055f7f9
rsimple <texturered@gmail.com>
2023-12-28 12:55:37 +0100

    eco: fix types

--- src/types/global.d.ts
+++ src/types/global.d.ts
@@ -25,7 +25,7 @@ declare type ValidateFn = (value: string | number | undefined | string[]) => str
 
 declare type InputType = 'number' | 'text' | 'tel' | 'search' | 'password' | 'email' | 'search' | 'url'
 
-declare module '@whitespots/ui-kit/dist/assets/icons/*' {
+declare module '@texturered/eco-vue-js/dist/assets/icons/*' {
   import type {FunctionalComponent, SVGAttributes} from 'vue'
   const src: FunctionalComponent<SVGAttributes>
   export default src
