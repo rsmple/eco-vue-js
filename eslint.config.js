03e6a4a463a21741f563e4d9ce554711e2608a53
Rus P <texturered@gmail.com>
2025-07-05 23:07:02 +0200

    ui-kit: update dependencies

--- eslint.config.js
+++ eslint.config.js
@@ -3,12 +3,12 @@ import recommended from './package/eslint/recommended.js'
 export default [
   {
     name: 'app/files-to-lint',
-    files: ['**/*.{ts,js,mts,tsx,vue,json}'],
+    files: ['**/*.{ts,js,mts,tsx,vue,json,svg}'],
   },
 
   {
     name: 'app/files-to-ignore',
-    ignores: ['**/dist/**', '**/dist-ssr/**', '**/coverage/**', '**/src/imports/**'],
+    ignores: ['**/dist/**', '**/dist-temp/**', '**/src/imports/**'],
   },
 
   ...recommended({
