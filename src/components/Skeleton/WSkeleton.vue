3894bb38a38f60cdcd0b2df1a0a1ec1ee309fc29
Rus P <texturered@gmail.com>
2025-09-21 21:10:54 +0200

    ui-kit: fix skeleton for ssr

--- src/components/Skeleton/WSkeleton.vue
+++ src/components/Skeleton/WSkeleton.vue
@@ -1,8 +1,25 @@
 <template>
   <div
     class="bg-primary-light dark:bg-primary-darkest w-skeleton w-skeleton-opacity-50 dark:w-skeleton-opacity-5 before:animate-ticker w-option-has-bg"
-    :style="{'--skeleton-width-internal': (Math.round(Math.random() * 40) + 40) + '%'}"
+    :style="{'--skeleton-width-internal': skeletonWidth + '%'}"
   >
     <slot />
   </div>
 </template>
+
+<script lang="ts" setup>
+import {computed, useId} from 'vue'
+
+const id = useId()
+
+const skeletonWidth = computed(() => {
+  let hash = 0
+  for (let i = 0; i < id.length; i++) {
+    const char = id.charCodeAt(i)
+    hash = ((hash << 5) - hash) + char
+    hash = hash & hash
+  }
+
+  return Math.abs(hash % 41) + 40 // 40-80%
+})
+</script>
