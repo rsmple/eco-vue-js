391f086b1afc6cdd2f9ddb819cbcb1424c747e61
Rus P <texturered@gmail.com>
2025-10-16 01:55:42 +0200

    ui-kit: add code-inline base

--- package/tailwind-base/plugins/text.ts
+++ package/tailwind-base/plugins/text.ts
@@ -19,6 +19,33 @@ const pluginDefault = plugin(function ({addBase, theme, config}) {
       '-webkit-text-security': 'disc',
     },
   })
+
+  addBase({
+    '.code-inline': {
+      fontFamily: theme('fontFamily.mono'),
+      paddingLeft: theme('spacing.1'),
+      paddingRight: theme('spacing.1'),
+      backgroundColor: 'rgb(229 231 235 / 0.5)',
+      borderRadius: theme('borderRadius.DEFAULT'),
+      userSelect: 'text',
+      fontWeight: theme('fontWeight.normal'),
+      [config('darkMode')[1][0]]: {
+        backgroundColor: 'rgb(75 85 99 / 0.5)',
+      },
+    },
+    '.code-inline.bg-positive': {
+      backgroundColor: `${theme('colors.positive')}4d`,
+      [config('darkMode')[1][0]]: {
+        backgroundColor: `${theme('colors.positive-dark')}4d`,
+      },
+    },
+    '.code-inline.bg-negative': {
+      backgroundColor: `${theme('colors.negative')}4d`,
+      [config('darkMode')[1][0]]: {
+        backgroundColor: `${theme('colors.negative-dark')}4d`,
+      },
+    },
+  })
 })
 
 export default pluginDefault
